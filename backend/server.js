8d226c4e8e47bd515840b94de3f09b392dd25986
irtizapanjwani <scig26806@gmail.com>
2025-10-19 21:24:41 +0500

    Rate-Limiting setup

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 
 import productRoutes from './routes/productRoutes.js'
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -16,6 +17,35 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, { requested: 1 })
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                res.writeHead(429, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Too Many Requests" }));
+            } else if (decision.reason.isBot()) {
+                res.writeHead(403, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "No bots allowed" }));
+            } else {
+                res.writeHead(403, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "Forbidden" }));
+            }
+            return
+        }
+
+        if (decision.results.some(isSpoofedBot)) {
+            res.writeHead(403, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Speefed bot detected" }));
+        }
+
+        next();
+    } catch (error) {
+        console.log("Arcjet error",error);
+        next(error);
+    }
+})
 
 app.use('/api/products', productRoutes)
 
