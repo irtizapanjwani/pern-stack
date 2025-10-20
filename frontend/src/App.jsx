69792efc002f8fd4e099965a71c06433fd3355da
irtizapanjwani <scig26806@gmail.com>
2025-10-20 19:50:27 +0500

    Navbar Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom"
 function App() {
 
   return (
-      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme="forest">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
